0791c227a5b42b2732722f34ce5d5fb869566420
HudaOmer <hudaoe175@gmail.com>
2024-07-23 12:02:16 -0700

    Solve task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-console.log("Welcome to Holberton School, what is your name?");
 
-rl.question('', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
