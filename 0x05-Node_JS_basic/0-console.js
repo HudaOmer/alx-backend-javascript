ea5a91c885b68cc6f81a4b0f5d023ac4b8cc8fab
HudaOmer <hudaoe175@gmail.com>
2024-07-23 10:17:51 -0700

    Solve task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,8 @@
-// create a function named displayMessage that prints
-// in STDOUT the string argument
+/**
+ * Creates a function named displayMessage that prints in STDOUT the string argument.
+ * @param {string} message - The message to be displayed.
+ */
+
 
 function displayMessage(message) {
 	console.log(message);
