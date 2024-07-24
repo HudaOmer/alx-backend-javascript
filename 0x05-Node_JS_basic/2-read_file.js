9e76218ca67bead37485485bcf28a236dfabb0b6
HudaOmer <hudaoe175@gmail.com>
2024-07-23 18:33:00 -0700

    Solve Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,15 +7,12 @@ function countStudents(fileName) {
   let length = 0;
 
   try {
-
     const fileContents = fs.readFileSync(fileName, 'utf-8');
     const lines = fileContents.toString().split('\n');
-
     for (let i = 0; i < lines.length; i += 1) {
       if (lines[i]) {
         length += 1;
         const field = lines[i].toString().split(',');
-
         if (Object.prototype.hasOwnProperty.call(students, field[3])) {
           students[field[3]].push(field[0]);
         } else {
