22a657fd2464725c466e320b37072073d596cde2
HudaOmer <hudaoe175@gmail.com>
2024-07-25 19:09:58 -0700

    Solve Task 1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -8,6 +8,9 @@ describe("calculateNumber()", function() {
       const res = calculateNumber("SUM", 1, 2);
       assert.strictEqual(res, 3);
     });
+    it('equal positive numbers (alternate)', () => {
+      assert.strictEqual(calculateNumber('SUM', 2.3, 1.8), 4);
+    });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("SUBTRACT", 1.4, 2.2);
       assert.strictEqual(res, -1);
@@ -15,6 +18,9 @@ describe("calculateNumber()", function() {
     it(`checking if numbers round`, function() {
       const res = calculateNumber("SUBTRACT", 4.9, 2.7);
       assert.strictEqual(res, 2);
+    });
+    it('equal negative numbers (alternate)', () => {
+      assert.strictEqual(calculateNumber('SUM', -2.3, -1.8), -4);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 4, 2);
