daf17c9a06fff0ddbec3e2ea208f47f1551604df
HudaOmer <hudaoe175@gmail.com>
2024-07-25 19:12:49 -0700

    Solve Task 2

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -2,6 +2,7 @@ function calculateNumber(type, a, b) {
     const an = Math.round(a);
     const bn = Math.round(b);
     let c = 0;
+    
     switch (type) {
     case 'SUM':
 	c = an + bn;
@@ -17,6 +18,7 @@ function calculateNumber(type, a, b) {
       }
       break;
     }
+
     return c;
 }
 
