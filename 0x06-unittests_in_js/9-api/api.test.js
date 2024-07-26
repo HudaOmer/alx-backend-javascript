cea16f68422896aa02d49d5d74098e0af175da4b
HudaOmer <hudaoe175@gmail.com>
2024-07-25 19:34:56 -0700

    Solve Task 9

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -7,13 +7,13 @@ describe("Index page", function() {
 	url: "http://localhost:7865/",
 	method: "GET"
     }
-    it("check correct status code", function(done) {
+    it("Check the correct status code", function(done) {
 	request(options, function(err, res, body) {
 	    expect(res.statusCode).to.equal(200);
 	    done();
 	});
     });
-    it("check correct content", function(done) {
+    it("Check the correct content", function(done) {
 	request(options, function(err, res, body) {
 	    expect(body).to.equal("Welcome to the payment system");
 	    done();
@@ -22,19 +22,19 @@ describe("Index page", function() {
 });
 
 describe("Cart page", function() {
-    it("check correct status code for correct url", function(done) {
+    it("Check the correct status code for correct url", function(done) {
 	request.get("http://localhost:7865/cart/12", function(err, res, body) {
 	    expect(res.statusCode).to.equal(200);
 	    done();
 	});
     });
-    it("check correct content for correct url", function(done) {
+    it("Check the correct content for correct url", function(done) {
 	request.get("http://localhost:7865/cart/12", function(err, res, body) {
 	    expect(body).to.contain("Payment methods for cart 12");
 	    done();
 	});
     });
-    it("check correct status code for incorrect url", function(done) {
+    it("Check the correct status code for incorrect url", function(done) {
 	request.get("http://localhost:7865/cart/kim", function(err, res, body) {
 	    expect(res.statusCode).to.equal(404);
 	    done();
